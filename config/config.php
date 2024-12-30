920d17815c9a22d6a60b9ac59d08e78155380c59
Kamran Zafar <kamranzafar4343@gmail.com>
2024-12-30 22:39:35 +0500

    created config page

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-define("DB_HOST", 'localhost');
-define("DB_USER", 'root');
-define("DB_PASS", '');
-define("DB_NAME", 'joblister');
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "joblister");
 
-define("SITE_TITLE", 'JobLister');
+define("SITE_TITLE", "JobLister");
 ?>
